2a92ace71559ee9ffbd79ed391e61839065af1cf
Sven Wagener <sven@awesome.ug>
2019-11-08 11:52:41 +0100

    Added missing scripts

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,112 +1,108 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    const OBSWebSocket = require('obs-websocket-js');
+  const OBSWebSocket = require("obs-websocket-js");
 
-    class OBSConnection {
-        
-        constructor( host = 'localhost', port = '4444', password = '' ) {
-            this.host     = host;
-            this.port     = port;
-            this.password = password;
-        }
-
-        connect() {
-            this.connection = new OBSWebSocket();
-            return this.connection.connect(
-                { 
-                    address: this.host + ':' + this.port,
-                    password: this.password 
-                }
-            );
-        }
+  class OBSConnection {
+    constructor(host = "localhost", port = "4444", password = "") {
+      this.host = host;
+      this.port = port;
+      this.password = password;
     }
 
-    class OBSSource {
+    connect() {
+      this.connection = new OBSWebSocket();
+      return this.connection.connect({
+        address: this.host + ":" + this.port,
+        password: this.password
+      });
+    }
+  }
 
-        constructor( connection, sourceName ) {
-            this.connection = connection;
-            this.sourceName = sourceName;
-        }
-
-        getSettings() {
-            return this.connection.send('GetSourceSettings',{
-                'sourceName': this.sourceName,
-            });
-        }
-
-        setSetting(name, value) {
-            return this.connection.send('SetSourceSettings',{
-                'sourceName': this.sourceName,
-                'sourceSettings': { name: value },
-            });
-        }
+  class OBSSource {
+    constructor(connection, sourceName) {
+      this.connection = connection;
+      this.sourceName = sourceName;
     }
 
-    // Example scripts
-
-    const obsCon = new OBSConnection('localhost', 4444, '1234567890');
-    obsCon.connect().then( data => {
-        const obsSrc = new OBSSource( obsCon, 'Name' );
-        obsSrc.setSetting('text', 'Jo man!');
-    }).catch( err => {
-        console.log(err);
-    });
-
-    /*
-    const obs = new OBSWebSocket();
-
-    obs.connect({ address: 'localhost:4444', password: '1234567890' }).then(() => {
-        console.log('Success! Connected.');
-        obs.disconnect();
-    }).catch( err => {
-        console.log('error');
-    });
-
-    let setSourceSettings = function ( obs ) {
-        obs.send('SetSourceSettings',{
-            'sourceName': 'Name',
-            'sourceSettings': { text: 'Dett iss mein Text' },
-        }).then( data => {
-            console.log( data );
-        }).catch( err => {
-            console.log( err );
-        });
+    getSettings() {
+      return this.connection.send("GetSourceSettings", {
+        sourceName: this.sourceName
+      });
     }
 
-    let getSourceSettings = function ( obs ) {
-        obs.send('GetSourceSettings',{
-            'sourceName': 'Name',
-        }).then( data => {
-            console.log( data );
-        }).catch( err => {
-            console.log( err );
-        });
+    setSetting(name, value) {
+      return this.connection.send("SetSourceSettings", {
+        sourceName: this.sourceName,
+        sourceSettings: { name: value }
+      });
     }
+  }
 
-    let getSources = function ( obs ) {
-        obs.send('GetCurrentScene').then( data => {
-            console.log( data );
-        }).catch( err => {
-            console.log( err );
-        });
-    }
+  // Example scripts
 
-    let startStream = function ( obs ) {
-        obs.send('StartStreaming').then( data => {
-            console.log( data );
-        }).catch( err => {
-            console.log( err );
-        });
-    }
+  const obsCon = new OBSConnection('localhost', 4444, '1234567890');
+  obsCon.connect().then( data => {
+      const obsSrc = new OBSSource( obsCon, 'Name' );
+      obsSrc.setSetting('text', 'Jo man!');
+  }).catch( err => {
+      console.log(err);
+  });
 
-    let stopStream = function ( obs ) {
-        obs.send('StopStreaming').then( data => {
-            console.log( data );
-        }).catch( err => {
-            console.log( err );
-        });
-    }
-    */
+  /*
+  const obs = new OBSWebSocket();
+
+  obs.connect({ address: 'localhost:4444', password: '1234567890' }).then(() => {
+      console.log('Success! Connected.');
+      obs.disconnect();
+  }).catch( err => {
+      console.log('error');
+  });
+
+  let setSourceSettings = function ( obs ) {
+      obs.send('SetSourceSettings',{
+          'sourceName': 'Name',
+          'sourceSettings': { text: 'Dett iss mein Text' },
+      }).then( data => {
+          console.log( data );
+      }).catch( err => {
+          console.log( err );
+      });
+  }
+
+  let getSourceSettings = function ( obs ) {
+      obs.send('GetSourceSettings',{
+          'sourceName': 'Name',
+      }).then( data => {
+          console.log( data );
+      }).catch( err => {
+          console.log( err );
+      });
+  }
+
+  let getSources = function ( obs ) {
+      obs.send('GetCurrentScene').then( data => {
+          console.log( data );
+      }).catch( err => {
+          console.log( err );
+      });
+  }
+
+  let startStream = function ( obs ) {
+      obs.send('StartStreaming').then( data => {
+          console.log( data );
+      }).catch( err => {
+          console.log( err );
+      });
+  }
+
+  let stopStream = function ( obs ) {
+      obs.send('StopStreaming').then( data => {
+          console.log( data );
+      }).catch( err => {
+          console.log( err );
+      });
+  }
+  */
 
 }());
